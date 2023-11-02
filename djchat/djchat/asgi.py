358574a8a3c59cf2375a6dc345f9e5bbdffdd0ad
Truong Hong Loc <hongloc2206@gmail.com>
2023-11-03 00:13:16 +0700

    update asgi

--- djchat/djchat/asgi.py
+++ djchat/djchat/asgi.py
@@ -44,12 +44,19 @@ django.setup()
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
 import chat.routing
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-     "websocket": AuthMiddlewareStack(
+    #  "websocket": AuthMiddlewareStack(
+    #      URLRouter(
+    #         chat.routing.websocket_urlpatterns
+    #     )
+    # ),
+     "websocket": AllowedHostsOriginValidator(AuthMiddlewareStack(
          URLRouter(
             chat.routing.websocket_urlpatterns
         )
-    ),
+    )),
 })
